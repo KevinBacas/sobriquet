028b678592f0fd2fc19b8476b9c65174a012994b
KevinBacas <bacas.kevin@hotmail.fr>
2015-12-16 19:52:51 +0100

    Added command-line-args + CLI

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
-import { updateTransitionTable } from './Markov.js';
+import commandLineArgs from 'command-line-args';
+import { updateTransitionTable, generateRandomWord } from './Markov.js';
 
-let database = {
-  transitionTable: []
-};
-let words = ['abc'];
-database = updateTransitionTable(database, words);
+let cli = commandLineArgs([
+  { name: 'words', type: String, multiple: true, defaultOption: true, defaultValue: [], description: 'List of words injected to the Markov chain' },
+  { name: 'number', alias: 'n', type: Number, defaultValue: 10, description: 'Number of words you want to generate' },
+  { name: 'start', alias: 's', type: String, defaultValue: 'a', description: 'The starting letter' },
+  { name: 'help', alias: 'h', type: Boolean, defaultValue: false, description: 'Display the usage of the CLI' }
+]);
+
+let options = cli.parse();
+
+if (options.help) {
+  console.log(cli.getUsage());
+} else {
+  let database = {
+    transitionTable: []
+  };
+  database = updateTransitionTable(database, options.words);
+  console.log("Random word(s) generated:");
+  for (let i = 0; i < options.number; ++i) {
+    console.log(generateRandomWord(database.transitionTable, options.start));
+  }
+}

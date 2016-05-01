ddf201aa7081f265a1ed4ec4b2a6847ad9058aaa
Kevin Bacas <bacas.kevin@hotmail.fr>
2016-05-01 12:15:46 +0200

    Fix linting errors

--- src/main.js
+++ src/main.js
@@ -1,24 +1,44 @@
-import './object-assign-polyfill';
 import commandLineArgs from 'command-line-args';
 import { updateTransitionTable, generateRandomWord } from './Markov.js';
 
-let cli = commandLineArgs([
-  { name: 'words', type: String, multiple: true, defaultOption: true, defaultValue: [], description: 'List of words injected to the Markov chain' },
-  { name: 'number', alias: 'n', type: Number, defaultValue: 10, description: 'Number of words you want to generate' },
-  { name: 'start', alias: 's', type: String, defaultValue: '\0', description: 'The starting letter' },
-  { name: 'help', alias: 'h', type: Boolean, defaultValue: false, description: 'Display the usage of the CLI' }
+const cli = commandLineArgs([
+  {
+    name: 'words',
+    type: String,
+    multiple: true,
+    defaultOption: true,
+    defaultValue: [],
+    description: 'List of words injected to the Markov chain',
+  }, {
+    name: 'number',
+    alias: 'n', type:
+    Number, defaultValue: 10,
+    description: 'Number of words you want to generate',
+  }, {
+    name: 'start',
+    alias: 's',
+    type: String,
+    defaultValue: '\0',
+    description: 'The starting letter',
+  }, {
+    name: 'help',
+    alias: 'h',
+    type: Boolean,
+    defaultValue: false,
+    description: 'Display the usage of the CLI',
+  },
 ]);
 
-let options = cli.parse();
+const options = cli.parse();
 
 if (options.help) {
   console.log(cli.getUsage());
 } else {
   let database = {
-    transitionTable: []
+    transitionTable: [],
   };
   database = updateTransitionTable(database, options.words);
-  console.log("Random word(s) generated:");
+  console.log('Random word(s) generated:');
   for (let i = 0; i < options.number; ++i) {
     console.log(generateRandomWord(database.transitionTable, options.start));
   }
